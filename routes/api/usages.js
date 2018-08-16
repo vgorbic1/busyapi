cf5d3daed38e7aa6258d81ea5e87a6afaa780331
Vladislav Gorbich <vgorbic1@users.noreply.github.com>
2018-08-16 14:12:55 -0500

    comment log and make the request async

--- routes/api/usages.js
+++ routes/api/usages.js
@@ -1,11 +1,11 @@
 module.exports = function(app){
-    app.post('/api/usages', function(req, res){
+    app.post('/api/usages', async function(req, res){
 
         // Store the supplied usage data
         app.usages.push(req.body);
 
         var usageId = app.usages.length;
-        console.log('Stored usage count: ' + usageId);
+        //console.log('Stored usage count: ' + usageId);
 
         res.status(201).json({'id':usageId});
     });
